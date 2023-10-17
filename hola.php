343979ccb43a760220bf9d3ef20df8e31d3de0a2
miguel <mbaecab821@g.educaand.es>
2023-10-17 09:23:45 +0200

    Añadido el comentario faltante

--- hola.php
+++ hola.php
@@ -1,5 +1,6 @@
 <?php
-//El resultado por defecto es hola mundo
+//Comentario Primero
+//El resultado por defecto es hola mundo, si no imprime un mensaje normal
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
 ?>
