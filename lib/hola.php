dc4dbf2c6e1e574520538a70b67d09210f581071
miguel <mbaecab821@g.educaand.es>
2023-10-17 09:45:20 +0200

    Hola usando la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
 <?php
 //Comentario Primero
 //El resultado por defecto es hola mundo, si no imprime un mensaje normal
+require('HolaMundo.php');
+
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+@print new HolaMundo($nombre);
 ?>
