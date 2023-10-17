8157f2c70c1e4af340a443bb9342cff7f2393a9d
miguel <mbaecab821@g.educaand.es>
2023-10-17 10:04:51 +0200

    Programa Interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -2,7 +2,7 @@
 //Comentario Primero
 //El resultado por defecto es hola mundo, si no imprime un mensaje normal
 require('HolaMundo.php');
-
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print new HolaMundo($nombre);
+print "Introduce tu nombre";
+$nombre = trim(fgets(STDIN));
+print "Hola $nombre\n";
 ?>
